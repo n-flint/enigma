697533bcaf189dc11883d016575e6672a17f3b8d
Noah Flint <noah.flint24@gmail.com>
2019-01-05 11:37:02 -0700

    created test_it_can_get_todays_date

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 class Enigma
 
 
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,13 +1,23 @@
+require 'simplecov'
+SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/enigma'
 
+#enigma = Enigma.new maybe put in setup method?
 class EnigmaTest < MiniTest::Test
 
   def test_it_exists
     enigma = Enigma.new
 
     assert_instance_of Enigma, enigma
+  end
+
+  def test_it_can_get_todays_date
+    enigma = Enigma.new
+
+    current_date = Date.today.strftime('%d%m%y')
+    assert_equal current_date, enigma.todays_date
   end
 
 
