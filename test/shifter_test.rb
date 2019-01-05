15f6d84873e27592c9e56a78c497e7b3a1641837
Noah Flint <noah.flint24@gmail.com>
2019-01-05 15:32:15 -0700

    created test_it_can_create_key_b

--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -15,10 +15,14 @@ class ShifterTest < MiniTest::Test
 
   def test_it_can_create_key_a
     shifter = Shifter.new
-
-
     #why does this not work with an integer as an argument
     assert_equal 02, shifter.shift_key_a("02715")
+  end
+
+  def test_it_can_create_key_b
+    shifter = Shifter.new
+
+    assert_equal 27, shifter.shift_key_b("02715")
   end
 
 
