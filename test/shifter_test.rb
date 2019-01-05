b926c48fc821a4b1ba23afa5b9591cad6d700600
Noah Flint <noah.flint24@gmail.com>
2019-01-05 14:17:19 -0700

    created test_it_can_shift_keys

--- test/shifter_test.rb
+++ test/shifter_test.rb
@@ -3,6 +3,7 @@ SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/shifter'
+require './lib/enigma'
 
 class ShifterTest < MiniTest::Test
 
@@ -10,6 +11,13 @@ class ShifterTest < MiniTest::Test
     shifter = Shifter.new
 
     assert_instance_of Shifter, shifter
+  end
+
+  def test_it_can_shift_keys
+    shifter = Shifter.new
+    expected = [02, 27, 71, 15]
+
+    assert_equal expected, shifter.shift_keys(02715)
   end
 
 
