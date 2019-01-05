0a7fa6a563b4ceb793f6136b0eb4b96a89143da1
Noah Flint <noah.flint24@gmail.com>
2019-01-05 13:27:41 -0700

    created character_set method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,12 +1,17 @@
 require 'date'
 
 class Enigma
+  attr_reader :character_set
 
+  def initialize
+    @character_set = ("a".."z").to_a << " "
+  end
 
   def todays_date
     Date.today.strftime('%d%m%y')
   end
 
+  #this will also generate a key of 00000, is that ok?
   def generate_key
     key = []
     5.times do
