52ee86bac02f03dbea61f09bb71592b2006cf787
Noah Flint <noah.flint24@gmail.com>
2019-01-05 17:26:33 -0700

    removed todays_date test from enigma_test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,14 +1,12 @@
 require 'date'
 
 class Enigma
-  attr_reader :character_set,
-              :todays_date
+  attr_reader :character_set
+
 
   def initialize
     @character_set = ("a".."z").to_a << " "
-    @todays_date = Date.today.strftime('%d%m%y')
   end
-
 
   #this will also generate a key of 00000, is that ok?
   def generate_key
