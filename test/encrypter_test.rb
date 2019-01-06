5d7aa9bf2f94df9e3882074c859a6133600ba4cb
Noah Flint <noah.flint24@gmail.com>
2019-01-06 13:34:02 -0700

    created test_it_exists

---
+++ test/encrypter_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+require './lib/shifter'
+
+class EncrypterTest < MiniTest::Test
+
+  def test_it_exists
+    encrypter = Encrypter.new
+
+    assert_instance_of Encrypter, encrypter
+  end
+
+
+end
