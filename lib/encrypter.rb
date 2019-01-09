c02a7218bff9952e0747e67e7ebfb49bd1a7abd8
Noah Flint <noah.flint24@gmail.com>
2019-01-09 09:52:12 -0700

    moved encrypt_method into encrypter

--- lib/encrypter.rb
+++ lib/encrypter.rb
@@ -1,13 +1,13 @@
-require './lib/encrypter'
+require './lib/enigma'
 
-class Encrypter
+class Encrypter < Enigma
 
-  # def encrypt_message(message, key, date)
-  #   final_shifts(key, date)
-  #   split_message(message, key, date).map do |letter|
-  #     rotate_letter_encrypt(letter, key, date)
-  #   end.join
-  # end
+  def encrypt_message(message, key, date)
+    final_shifts(key, date)
+    split_message(message, key, date).map do |letter|
+      rotate_letter_encrypt(letter, key, date)
+    end.join
+  end
 
 
 
