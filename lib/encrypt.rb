942628ebdb65c7c99f52445afc59d8d761f62192
Noah Flint <noah.flint24@gmail.com>
2019-01-09 15:12:26 -0700

    included quote in cli method

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -6,7 +6,7 @@ message.close
 
 enigma = Enigma.new
 encrypt = enigma.encrypt(read_message)
-joined = "Created #{ARGV[1]} with the key #{encrypt[:key]} and date #{encrypt[:date]}"
+joined = "Created '#{ARGV[1]}' with the key #{encrypt[:key]} and date #{encrypt[:date]}"
 output = File.open(ARGV[1], "w")
 output.write("#{encrypt[:encryption]}")
 output.close
