22cc2c7b84183df6108bc0487b523b944236a58f
Noah Flint <noah.flint24@gmail.com>
2019-01-09 11:53:27 -0700

    refactored keys class

--- lib/keys.rb
+++ lib/keys.rb
@@ -8,6 +8,23 @@ class Keys
     key.join.to_i
   end
 
+  def pad_key
+    generate_key.to_s.rjust(5, "0")
+  end
+
+  def key_pairs(key)
+    key.split(//).map do |key|
+    key.to_i
+    end
+  end
+
+  def combine_pairs(key)
+    pairs = key_pairs(key).each_cons(2).to_a
+    pairs.map do |pair|
+      pair.join.to_i
+    end
+  end
+
 
 
 
