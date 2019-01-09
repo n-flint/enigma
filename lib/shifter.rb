13c1888c452483e8f68554a9935dfd51b803a773
Noah Flint <noah.flint24@gmail.com>
2019-01-09 10:54:11 -0700

    moved offset methods to shifter class

--- lib/shifter.rb
+++ lib/shifter.rb
@@ -41,6 +41,22 @@ class Shifter < Enigma
     end
   end
 
+  def offset_a(key, date)
+    date_last_four(date)[0].to_i
+  end
+
+  def offset_b(key, date)
+    date_last_four(date)[1].to_i
+  end
+
+  def offset_c(key, date)
+    date_last_four(date)[2].to_i
+  end
+
+  def offset_d(key, date)
+    date_last_four(date)[3].to_i
+  end
+
 
 
 
