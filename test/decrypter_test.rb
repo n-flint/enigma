f43e85cfc8e8b25c2855425497925f31e1409523
Noah Flint <noah.flint24@gmail.com>
2019-01-09 10:01:59 -0700

    created test_it_exists

--- test/decrypter_test.rb
+++ test/decrypter_test.rb
@@ -3,11 +3,15 @@ SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/enigma'
-require './lib/encrypter'
+require './lib/decrypter'
 
 class EncrypterTest < MiniTest::Test
 
+  def test_it_exists
+    decrypter = Decrypter.new
 
+    assert_instance_of Decrypter, decrypter
+  end
 
 
 
