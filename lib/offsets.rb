4f1e19801e883a00914419f68fc75a2432b7d5ef
Noah Flint <noah.flint24@gmail.com>
2019-01-09 11:32:58 -0700

    moved offset methods into offsets class

--- lib/offsets.rb
+++ lib/offsets.rb
@@ -16,21 +16,21 @@ class Offsets
   end
 
 
-  # def offset_a(key, date)
-  #   date_last_four(date)[0].to_i
-  # end
-  #
-  # def offset_b(key, date)
-  #   date_last_four(date)[1].to_i
-  # end
-  #
-  # def offset_c(key, date)
-  #   date_last_four(date)[2].to_i
-  # end
-  #
-  # def offset_d(key, date)
-  #   date_last_four(date)[3].to_i
-  # end
+  def offset_a(key, date)
+    date_last_four(date)[0].to_i
+  end
+
+  def offset_b(key, date)
+    date_last_four(date)[1].to_i
+  end
+
+  def offset_c(key, date)
+    date_last_four(date)[2].to_i
+  end
+
+  def offset_d(key, date)
+    date_last_four(date)[3].to_i
+  end
 
 
 end
--- test/offsets_test.rb
+++ test/offsets_test.rb
@@ -31,13 +31,13 @@ class OffsetsTest < MiniTest::Test
     assert_equal [1, 0, 2, 5], offsets.date_last_four("040895")
   end
 
-  # def test_it_can_generate_offsets
-  #   offsets = Offsets.new
-  #
-  #   assert_equal 1, offsets.offset_a("02715", "040895")
-  #   assert_equal 0, offsets.offset_b("02715", "040895")
-  #   assert_equal 2, offsets.offset_c("02715", "040895")
-  #   assert_equal 5, offsets.offset_d("02715", "040895")
-  # end
+  def test_it_can_generate_offsets
+    offsets = Offsets.new
+
+    assert_equal 1, offsets.offset_a("02715", "040895")
+    assert_equal 0, offsets.offset_b("02715", "040895")
+    assert_equal 2, offsets.offset_c("02715", "040895")
+    assert_equal 5, offsets.offset_d("02715", "040895")
+  end
 
 end
