b1da8eadc689dcfa6cc80a3e0939d783a8253943
Noah Flint <noah.flint24@gmail.com>
2019-01-09 10:00:50 -0700

    created decrypter test file

---
+++ test/decrypter_test.rb
@@ -0,0 +1,15 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+require './lib/encrypter'
+
+class EncrypterTest < MiniTest::Test
+
+
+
+
+
+
+end
